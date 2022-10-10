324159ac554945ba3256bf508d090112b51502d4
bcstryker <bcstryker@gmail.com>
2022-10-10 17:23:03 -0400

    added some text to homepage for site context

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,16 @@ import {StrykerPage} from "../types";
 const Home: StrykerPage = () => {
   return (
     <>
-      <div className="block xl:flex mb-8 sm:mb-10">
-        <div className="w-full flex justify-center">
-          <h1 className="text-xl text-foreground-alt-100">Welcome!</h1>
+      <div className="w-full flex justify-center mb-16 mt-48">
+        <h1 className="text-xl text-foreground-alt-100">Welcome!</h1>
+      </div>
+      <div className="w-full flex justify-center">
+        <div className="w-2/3">
+          <h3 className="text-xl text-foreground-alt-100 whitespace-pre-wrap text-justify">
+            {
+              "Hello Friend, \n\n\tMy name is Brandon and this is my site. I do not know how you ended up here but please be aware, this site is under development. The word game is fun, but not much else here works yet. Maybe play some not wordle and check back some other time."
+            }
+          </h3>
         </div>
       </div>
     </>

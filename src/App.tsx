80a449631e68e13f3af8e43084886c1705dceb70
Brandon Stryker <24856006+bcstryker@users.noreply.github.com>
2024-03-25 14:03:02 -0400

    forgot to add files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-[100vw] h-[100vh] flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-red-600">
+        Parked
+      </h1>
+    </div>
+  );  
+}
+
+export default App;

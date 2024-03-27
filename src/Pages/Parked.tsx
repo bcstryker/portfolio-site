7e89250dd51dbe634686823e7d91364e66c90bf9
Brandon Stryker <24856006+bcstryker@users.noreply.github.com>
2024-03-27 15:56:02 -0400

    made default page "Parked"

---
+++ src/Pages/Parked.tsx
@@ -0,0 +1,9 @@
+const Parked = () => (
+    <div className="w-[100vw] h-[100vh] flex items-center justify-center">
+        <h1 className="text-6xl font-bold text-red-600">
+        Parked
+        </h1>
+    </div>
+)
+
+export default Parked;
